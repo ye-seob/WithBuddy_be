408b78f96cd28e7e4bdb74885aaf5fb54fdba23b
ye-seob <jakalob103@naver.com>
2024-09-03 12:57:02 +0900

    그룹 매칭으로 변경

--- src/controllers/matchController.ts
+++ src/controllers/matchController.ts
@@ -19,40 +19,10 @@ export const match = async (req: Request, res: Response) => {
       regex = /^[0-9]*0(2[0-9]|3[0-9])$/;
     } else if (lastThree >= 40 && lastThree <= 59) {
       regex = /^[0-9]*0(4[0-9]|5[0-9])$/;
-    } else if (lastThree >= 60 && lastThree <= 79) {
-      regex = /^[0-9]*0(6[0-9]|7[0-9])$/;
-    } else if (lastThree >= 80 && lastThree <= 99) {
-      regex = /^[0-9]*0(8[0-9]|9[0-9])$/;
-    } else if (lastThree >= 100 && lastThree <= 119) {
-      regex = /^[0-9]*1(0[0-9]|1[0-9])$/;
-    } else if (lastThree >= 120 && lastThree <= 139) {
-      regex = /^[0-9]*1(2[0-9]|3[0-9])$/;
-    } else if (lastThree >= 140 && lastThree <= 159) {
-      regex = /^[0-9]*1(4[0-9]|5[0-9])$/;
-    } else if (lastThree >= 160 && lastThree <= 179) {
-      regex = /^[0-9]*1(6[0-9]|7[0-9])$/;
-    } else if (lastThree >= 180 && lastThree <= 199) {
-      regex = /^[0-9]*1(8[0-9]|9[0-9])$/;
-    } else if (lastThree >= 200 && lastThree <= 219) {
-      regex = /^[0-9]*2(0[0-9]|1[0-9])$/;
-    } else if (lastThree >= 220 && lastThree <= 239) {
-      regex = /^[0-9]*2(2[0-9]|3[0-9])$/;
-    } else if (lastThree >= 240 && lastThree <= 259) {
-      regex = /^[0-9]*2(4[0-9]|5[0-9])$/;
-    } else if (lastThree >= 260 && lastThree <= 279) {
-      regex = /^[0-9]*2(6[0-9]|7[0-9])$/;
-    } else if (lastThree >= 280 && lastThree <= 299) {
-      regex = /^[0-9]*2(8[0-9]|9[0-9])$/;
-    } else if (lastThree >= 300 && lastThree <= 319) {
-      regex = /^[0-9]*3(0[0-9]|1[0-9])$/;
-    } else if (lastThree >= 320 && lastThree <= 339) {
-      regex = /^[0-9]*3(2[0-9]|3[0-9])$/;
-    } else if (lastThree >= 340 && lastThree <= 359) {
-      regex = /^[0-9]*3(4[0-9]|5[0-9])$/;
-    } else if (lastThree >= 360 && lastThree <= 379) {
-      regex = /^[0-9]*3(6[0-9]|7[0-9])$/;
-    } else if (lastThree >= 380 && lastThree <= 399) {
-      regex = /^[0-9]*3(8[0-9]|9[0-9])$/;
+    } else if (lastThree >= 60 && lastThree <= 90) {
+      regex = /^0(6[0-9]|7[0-9]|8[0-9]|90)$/;
+    } else if (lastThree > 90 && lastThree <= 200) {
+      regex = /^(9[1-9]|1[0-9][0-9]|200)$/;
     } else {
       return res.status(400).send("적절한 학번 구간에 속하지 않습니다.");
     }
