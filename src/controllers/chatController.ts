0d03ed4cc1b0d93fb19a0e67cd5af38f171c4022
ye-seob <jakalob103@naver.com>
2024-09-11 12:12:28 +0900

    알림 기능 -2

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -1,3 +1,4 @@
+// 백엔드 코드 수정 (notification 이벤트 추가)
 import { Server, Socket } from "socket.io";
 import ChatMessage from "../models/chatModel";
 
@@ -33,6 +34,13 @@ export default function (io: Server): void {
       await chatMessage.save();
 
       io.to(room).emit("chat message", chatMessage);
+
+      // 새 메시지가 올 때마다 알림 이벤트 전송
+      io.to(room).emit("notification", {
+        title: "새로운 메시지",
+        message: `${name}님으로부터 새로운 메시지가 도착했습니다.`,
+        timestamp: new Date(),
+      });
     });
 
     socket.on("disconnect", () => {});
